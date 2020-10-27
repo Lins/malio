6c19550a95ff867cd107268f0aeb58b322163d28
Lins <lingchen89@gmail.com>
2020-10-27 15:51:15 +0800

    Create serverinstall.sh

---
+++ serverinstall.sh
@@ -0,0 +1,5 @@
+mkdir v2ray-agent && \
+cd v2ray-agent && \
+curl https://raw.githubusercontent.com/alexalexmaestro/v2ray-sspanel-rico_cracked/master/install.sh -o install.sh && \
+chmod +x install.sh && \
+bash install.sh
